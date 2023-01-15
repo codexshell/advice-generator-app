7fb93f9f9b966871d5dc6add63eb8da1f539de0f
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-15 08:12:49 +0300

    fix: stop firefox browswer from caching api responses
    firefox is caching api responses and reusing them for subsequest
    responses. After attempting to solve the problem by tweaking the axios
    configurations i was unsuccessfull
    
    Switched to using the fetch api which proved quite resourceful

--- src/apis/getQuote.js
+++ src/apis/getQuote.js
@@ -1,13 +1,8 @@
-import axios from "axios";
 const baseUrl = "https://api.adviceslip.com/advice";
 
 const getQuote = async () => {
-  try {
-    const response = await axios.get(baseUrl);
-    return response.data.slip;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await fetch(baseUrl, { cache: "no-cache" });
+  const data = await response.json();
+  return data.slip;
 };
-
 export default getQuote;
